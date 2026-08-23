991035c04db7e0efa410cbdf0a142363db3c0aa1
Bhargav <bhargavraju015@gmail.com>
2026-08-23 09:23:00 +0530

    Initial commit

---
+++ src/main/java/com/britespark/reachreminder/ReachReminderApplication.java
@@ -0,0 +1,13 @@
+package com.britespark.reachreminder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ReachReminderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ReachReminderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/britespark/reachreminder/ReachReminderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.britespark.reachreminder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ReachReminderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
